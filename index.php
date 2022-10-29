f557e149b7ccd7f06a96002b1bb3216e37d4a1f7
nicholemaldonado4 <nicholemaldonado4@gmail.com>
2022-10-28 21:52:58 -0600

    Added initial files for the main page

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Wedding Board</title>
+  
+  <link
+     href="https://fonts.googleapis.com/css?family=Oranienbaum"
+     rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+  <link rel="stylesheet" href="css/header.css">
+  <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+</body>
