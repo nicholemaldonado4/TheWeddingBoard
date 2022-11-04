b8face794e9d4f8a60c52689c4ea9578b5d0fb2c
nicholemaldonado4 <nicholemaldonado4@gmail.com>
2022-11-03 21:35:48 -0600

    Added bare bones implementation for login page

---
+++ login/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Wedding Board | Log In</title>
+  
+  <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Lobster Two">
+  <link href="/css/login/index.css" rel="stylesheet">
+</head>
+<body>
+  <div><h1><a href="/">Wedding Board</a></h1></div>
+  <div class="login-box">
+    <h2>Login</h2>
+    <form>
+      <div>
+      <label for="username">Username</label>
+      <input type="text" name="username">
+      </div>
+      <div>
+      <label for="password">Password</label>
+      <input type="password">
+      </div>
+      <input type="submit">
+    </form>
+    <p>Need an account? <a href="/signup">Sign Up</a></p>
+  </div>
+</body>
--- php/header.php
+++ php/header.php
@@ -3,13 +3,13 @@
     <li><a class="logo-name" href="/">Wedding Board</a></li>
   </ul>
   
-  <ul class="right-nav" class="not-signed-in">
+  <ul class="right-nav" class="not-logged-in">
     <li><a href="/">Home</a></li>
     <li><a href="">Write a Message</a></li>
-    <li><a href="/signin">Sign In</a></li>
+    <li><a href="login">Login In</a></li>
   </ul>
   
-  <ul class="right-nav" class="signed-in">
+  <ul class="right-nav" class="logged-in">
     <li><a href="/">Home</a></li>
     <li><a href="">My Boards</a></li>
     <li>Welcome</li>
