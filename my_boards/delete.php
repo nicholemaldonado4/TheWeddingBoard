bb2a5eae8dc5c0aff92b37b2610c0238010eb2d0
nicholemaldonado4 <nicholemaldonado4@gmail.com>
2022-12-29 23:47:34 -0700

    Added functionality to delete a board

---
+++ my_boards/delete.php
@@ -0,0 +1,47 @@
+
+<?php
+
+include_once("../database.php");
+include_once("../session.php");
+$session = new Session();
+$session->redirectIfNotLoggedIn("../");
+
+function delete_board($session) {
+  if ($_SERVER["REQUEST_METHOD"] !== "GET") {
+    header("Location: ../");
+    exit();
+  }
+  
+  $_GET_lower = array_change_key_case($_GET, CASE_LOWER);
+  
+  # If user does not provide board name, ignore request.
+  if (!array_key_exists("board", $_GET)) {
+    header("Location: index.php");
+    exit();
+  }
+  
+  $username = $session->getUsername();
+  $board = $_GET["board"];
+  
+  $db = new Database();
+  $err  = $db->connect();
+  if (!empty($err)) {
+    $session->setFlashData(new FlashData("A problem occurred. Please try again."));
+    header("Location: index.php");
+    exit();
+  }
+  
+  // Delete table
+  $stmt = $db->getCon()->prepare("DELETE FROM WEDDING_BOARD WHERE WUsername=? AND WTitle=?");
+  $stmt->bind_param('ss', $username, $board);
+  if ($stmt->execute() == FALSE) {
+    $session->setFlashData(new FlashData("Unable to delete board $board."));
+    echo "Unable to execute";
+  }
+  header("Location: index.php");
+  exit();
+}
+
+delete_board($session);
+
+?>
--- my_boards/get_boards.php
+++ my_boards/get_boards.php
@@ -28,7 +28,7 @@ function get_boards($session) {
     echo "<td scope='row' data-label='Pin'>$pin</td>";
     echo "<td scope='row' data-label='Password'>$password</td>";
     echo "<td scope='row' data-label='Actions'><div><a href=customize?op=edit&board=$title>Edit</a>".
-      " $spacing <a href=delete?board=$title>Delete</a></div></td>";
+      " $spacing <a href=delete.php?board=$title>Delete</a></div></td>";
     echo "</tr>";
   }        
 }
