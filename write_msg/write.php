df754ab2ad5814330de3ab0f050710c05950228f
nicholemaldonado4 <nicholemaldonado4@gmail.com>
2023-02-10 22:45:48 -0700

    Added styling for image upload

--- write_msg/write.php
+++ write_msg/write.php
@@ -12,6 +12,7 @@
   
   <link rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Lobster Two|Assistant">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
   <link href="/css/write_msg/write.css" rel="stylesheet">
   <link href="/css/header.css" rel="stylesheet">
   
@@ -30,13 +31,18 @@
   <section class="msg-sect">
     <form>
       <div class="msg-div">
-        <label for="board_msg">Write a Message</label>
+        <h2 for="board_msg">Write a Message</h2>
         <textarea name="board_msg"></textarea>
         <p class="light-font">Char Left: <span id='char-left'>200</span></p>
       </div>
       <div>
-        <label for="board_img">Add an Image</label>
-        <input type="file" accept="image/png, image/jped" name="board_img">
+        <h2 for="board_img">Add an Image</h2>
+        <div class="previewer">
+          <img id="img-preview hidden">
+          <i class="fa-regular fa-image"></i>
+          <label for="board_img" class="input-btn">Upload Image</label>
+        </div>
+        <input type="file" accept="image/png, image/jped" name="file-img" id="board_img">
       </div>
       <input type="submit" value="Write to Board">
     </form>
